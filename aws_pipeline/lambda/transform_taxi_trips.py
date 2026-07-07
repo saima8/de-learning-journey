298ee592b1399807e30bebac7494b1cb7c8ce2b6
Saima Khan <tithi888khan@gmail.com>
2026-07-07 01:45:09 +0100

    feat: update Lambda to read bucket/key dynamically from S3 event

--- aws_pipeline/lambda/transform_taxi_trips.py
+++ aws_pipeline/lambda/transform_taxi_trips.py
@@ -1,18 +1,21 @@
 ﻿import boto3
 import csv
 import io
+import urllib.parse
 
 s3 = boto3.client('s3')
 
-RAW_BUCKET = 'nyc-taxi-raw-saima'
 PROCESSED_BUCKET = 'nyc-taxi-processed-saima'
-INPUT_KEY = 'landing/sample_trips.csv'
-OUTPUT_KEY = 'processed/clean_trips.csv'
 
 def lambda_handler(event, context):
-    print(f"Reading from s3://{RAW_BUCKET}/{INPUT_KEY}")
+    raw_bucket = event['Records'][0]['s3']['bucket']['name']
+    raw_key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key']
+    )
     
-    response = s3.get_object(Bucket=RAW_BUCKET, Key=INPUT_KEY)
+    print(f"Triggered by: s3://{raw_bucket}/{raw_key}")
+    
+    response = s3.get_object(Bucket=raw_bucket, Key=raw_key)
     raw_content = response['Body'].read().decode('utf-8')
     
     reader = csv.DictReader(io.StringIO(raw_content))
@@ -23,6 +26,9 @@ def lambda_handler(event, context):
     
     print(f"Raw rows: {raw_content.count(chr(10))} | Clean rows: {len(clean_rows)}")
     
+    filename = raw_key.split('/')[-1]
+    output_key = f"processed/clean_{filename}"
+    
     if clean_rows:
         output = io.StringIO()
         writer = csv.DictWriter(output, fieldnames=clean_rows[0].keys())
@@ -31,13 +37,13 @@ def lambda_handler(event, context):
         
         s3.put_object(
             Bucket=PROCESSED_BUCKET,
-            Key=OUTPUT_KEY,
+            Key=output_key,
             Body=output.getvalue().encode('utf-8'),
             ContentType='text/csv'
         )
-        print(f"Written to s3://{PROCESSED_BUCKET}/{OUTPUT_KEY}")
+        print(f"Written to s3://{PROCESSED_BUCKET}/{output_key}")
     
     return {
         'statusCode': 200,
-        'body': f"Done. {len(clean_rows)} clean rows written."
+        'body': f"Done. {len(clean_rows)} clean rows written to {output_key}"
     }
